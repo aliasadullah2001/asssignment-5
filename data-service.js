637053f3b6ad2668129e9272b73f4aec1dda503d
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-09 04:17:21 -0400

    updates

---
+++ data-service.js
@@ -0,0 +1,59 @@
+
+const fs = require ('fs');
+var employees = new Array();
+var departments = new Array();
+var exports = module.exports={};
+
+
+exports.initialize = ()=>{
+
+return new Promise((resolve, reject)=>{
+
+    fs.readFile('data/employees.json',(err,data)=>{
+        if (err){reject("Failure to read file employees.json!")
+    } else{
+        employees = JSON.parse(data);      
+    }});
+        fs.readFile('data/departments.json',(err,data)=>{
+    
+                if (err){reject("Failure to read file employees.json!")
+            }  else{ departments = JSON.parse(data);
+            }
+               
+                
+            })
+            resolve();
+        })
+
+    };
+
+
+ 
+exports.geAlltEmployees= ()=>{
+    return new Promise((resolve , reject)=>{
+        if (employees.length != 0)
+        resolve(employees);
+        reject("no employees");
+    })
+}
+
+
+exports.getDepartments = ()=>{
+    return new promise((resolve, reject)=>{
+        if(departments.length!=0)
+        resolve(departments);
+        else
+        reject("no departments");
+    
+    })
+};
+
+exports.getManagers= ()=>{
+ return new promise ((resolve, reject)=>{
+var managers= employees.filter(employee =>employee.ismanager==true);
+if(managers.length==0){
+    reject("no managers");
+}
+resolve(managers);
+ })
+};
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var HTTP_PORT = process.env.PORT || 8080;
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: _____Ali Asadullah_____ Student ID: _174606210
+__ Date: __10/09/2022__
+*
+* Your app’s URL (from Cyclic) :https://faithful-coat-bear.cyclic.app/
+*
+*************************************************************************/ 
+const fs= require("fs");
 var express = require("express");
+var HTTP_PORT = process.env.PORT || 8080;
+var data = require('./data-service');
 var app = express();
 var path = require("path");
-//var data = require("data-service.js")
 app.use(express.static('public'));
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
@@ -19,12 +33,32 @@ app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "views/about.html"));
 }
 ,
-app.get("/deaprtments", (req, res) => {
-  // send the html view with our form to the client
-  res.sendFile(path.join(__dirname, "data/departments.json"));
+app.get("/employees",function (req,res) {
+data.geAlltEmployees()
+.then((data) => res.json(data))
+.catch((err)=> res.json({"message": err}))
 }
-
-)));
+,
+app.get("/managers",function (req,res) {
+  data.getManagers()
+  .then((data) => res.json(data))
+  .catch((err)=> res.json({"message": err}))
+  }
+  ,
+  app.get("/departments",function (req,res) {
+    data.getDepartments()
+    .then((data) => res.json(data))
+    .catch((err)=> res.json({"message": err}))
+    }
+  
+)))));
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+
+data.initialize()
+.then((data)=>{
+  app.listen(HTTP_PORT, onHttpStart);
+})
+.catch(()=>{
+  console.log("error")
+})
