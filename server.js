28f5ef139f24c0cb27c889643e831243e73b3621
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-09 00:23:02 -0400

    changes

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-
+var path = require("path");
+//var data = require("data-service.js")
+app.use(express.static('public'));
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+  }
+   
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+    // send the html view with our form to the client
+    res.sendFile(path.join(__dirname, "views/home.html"));
+}
+,
+app.get("/about", (req, res) => {
+  // send the html view with our form to the client
+  res.sendFile(path.join(__dirname, "views/about.html"));
+}
+,
+app.get("/deaprtments", (req, res) => {
+  // send the html view with our form to the client
+  res.sendFile(path.join(__dirname, "data/departments.json"));
+}
+
+)));
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, onHttpStart);
