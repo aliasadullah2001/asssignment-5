b5e6eef235ed813a1fe4c575aa81487bd2f0a82e
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-12 18:03:26 -0400

    update

--- data-service.js
+++ data-service.js
@@ -10,51 +10,51 @@ exports.initialize = ()=>{
 return new Promise((resolve, reject)=>{
 
     fs.readFile('data/employees.json',(err,data)=>{
-        if (err){reject("Failure to read file!")
+        if (err){reject("Failure to read file!");
     } else{
         employees = JSON.parse(data);      
     }});
-        fs.readFile('data/departments.json',(err,data)=>{
+   fs.readFile('data/departments.json',(err,data)=>{
     
-                if (err){reject("Failure to read file!")
+        if (err){reject("Failure to read file!");
             }  else{ departments = JSON.parse(data);
             }
                
                 
-            })
+            });
             resolve();
-        })
+        });
 
-    };
+    }
 
 
  
-exports.geAlltEmployees= ()=>{
+exports.getDepartments= ()=>{
     return new Promise((resolve , reject)=>{
-        if (employees.length != 0)
-        resolve(employees);
+        if (departments.length != 0)
+        resolve(departments);
         reject("no results returned");
-    })
+    });
 }
 
 
-exports.getDepartments = ()=>{
-    return new promise((resolve, reject)=>{
-        if(departments.length==0)
-        reject("no results returned");
-      
-        else
-        resolve(departments);
-    
-    })
-};
+exports.geAlltEmployees = ()=>{
+    return new Promise((resolve, reject)=>{
+        if(employees.length !=0)
+        resolve(employees);
+        reject("no reuslts returned");
+    }
+    );
+}
+   
+
 
 exports.getManagers= ()=>{
- return new promise ((resolve, reject)=>{
+ return new Promise ((resolve, reject)=>{
 var managers= employees.filter(employee =>employee.ismanager==true);
 if(managers.length==0){
     reject("no results returned");
 }
 resolve(managers);
- })
-};
+ });
+}
