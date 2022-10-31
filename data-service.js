82178804264e444b302bc5d91f1c23a0b8272fc0
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-31 19:49:02 -0400

    changes

--- data-service.js
+++ data-service.js
@@ -49,12 +49,71 @@ exports.geAlltEmployees = ()=>{
    
 
 
-exports.getManagers= ()=>{
- return new Promise ((resolve, reject)=>{
-var managers= employees.filter(employee =>employee.ismanager==true);
-if(managers.length==0){
-    reject("no results returned");
+exports.getManagers = function(){
+    return new Promise((resolve, reject)=>{
+        let managers=[];
+       /* for (let i=0; i<employees.length; i++)
+            {
+                if (employees[i].isManager)
+                    managers.push(employees[i]);
+            }
+        */
+            employees.forEach(function (employee) {
+                if (employee.isManager) {
+                    managers.push(employee);
+                }
+            });
+            
+        if (managers.length>0)
+            resolve(managers);
+        else
+            reject("No results returned.");
+    });
 }
-resolve(managers);
- });
+exports.addEmployee = function(employeeData){
+    if(!employeeData.isManager) employeeData.isManager = false;
+    else employeeData.isManager = true;
+    employeeData.employeeNum = employees.length+1;
+    employees.push(employeeData);
+    return new Promise((resolve, reject) => {
+        resolve(employees);
+        if(employees.length == 0)
+        reject("no results returned");
+    });
+};
+
+exports.getEmployeesByStatus = function(status){
+    return new Promise((resolve, reject) => {
+        let filteredEmployees = employees.filter(employees => employees.status == status);
+        resolve(filteredEmployees);
+        if(filteredEmployees.length == 0)
+        reject("no results returned");
+    });
+}
+
+exports.getEmployeesByDepartment = function(department){
+    return new Promise((resolve, reject) => {
+        let filteredEmployees = employees.filter(employees => employees.department == department);
+        resolve(filteredEmployees);
+        if(filteredEmployees.length == 0)
+        reject("no results returned");
+    });
+}
+
+exports.getEmployeesByManager = function(manager){
+    return new Promise((resolve, reject) => {
+        let filteredEmployees = employees.filter(employees => employees.employeeManagerNum == manager);
+        resolve(filteredEmployees);
+        if(filteredEmployees.length == 0)
+        reject("no results returned");
+    });
+}
+
+exports.getEmployeesByNum = function(num){
+    return new Promise((resolve, reject) => {
+        let filteredEmployees = employees.filter(employees => employees.employeeNum == num);
+        resolve(filteredEmployees);
+        if(filteredEmployees.length == 0)
+        reject("no results returned");
+    });
 }
