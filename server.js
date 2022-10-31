e54d7f23d78924a9cee84550a4b46941e44ecfb3
aliasadullah2001 <ali.asadullah2001@gmail.com>
2022-10-31 19:46:28 -0400

    updates

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 /*************************************************************************
-* BTI325– Assignment 2
-* I declare that this assignment is my own work in accordance with Seneca Academic
-Policy. No part * of this assignment has been copied manually or electronically from any
-other source
+* BTI325– Assignment 3
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+No part of this assignment has been copied manually or electronically from any other source.
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: _____Ali Asadullah_____ Student ID: _174606210
-__ Date: __10/09/2022__
+* Name: ________Ali Asadullah____________________ Student ID: _____174606210_________ Date: ___10/31/2022____
 *
-* Your app’s URL (from Cyclic) :https://faithful-coat-bear.cyclic.app/
+* Your app’s URL (from  Heroku) that I can click to see your application:
+* ______________________________________________
 *
-*************************************************************************/ 
+*************************************************************************/
+
+var bodyParser = require("body-parser");
 const fs= require("fs");
 var express = require("express");
 var HTTP_PORT = process.env.PORT || 8080;
 var data = require('./data-service');
 var app = express();
 var path = require("path");
+var multer = require("multer");
+const storage = multer.diskStorage({
+  destination : "./public/images/uploaded", 
+  filename: function(req, file, cb){
+      cb(null, Date.now() + path.extname(file.originalname)); 
+  }
+
+});
+
+var upload = multer({storage : storage});
 app.use(express.static('public'));
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
@@ -33,10 +44,34 @@ app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "views/about.html"));
 }
 ,
-app.get("/employees",function (req,res) {
-data.geAlltEmployees()
-.then((data) => res.json(data))
-.catch((err)=> res.json({"message": err}))
+app.get("/employees/add", (req, res) => {
+  // send the html view with our form to the client
+  res.sendFile(path.join(__dirname, "views/addEmployee.html"));
+}
+,
+app.get("/images/add", (req, res) => {
+  // send the html view with our form to the client
+  res.sendFile(path.join(__dirname, "views//addImage.html"));
+}
+,
+app.get('/employees', (req, res) => {
+  if(req.query.status) {
+      data.getEmployeesByStatus(req.query.status)
+          .then((data) => res.json(data))
+          .catch((err) => res.json({"message": err}))
+  }else if(req.query.manager){
+      data.getEmployeesByManager(req.query.manager)
+          .then((data) => res.json(data))
+          .catch((err) => res.json({"message": err}))
+  }else if(req.query.department){
+      data.getEmployeesByDepartment(req.query.department)
+          .then((data) => res.json(data))
+          .catch((err) => res.json({"message": err}))
+  }else{
+      data.geAlltEmployees()
+          .then((data) => res.json(data))
+          .catch((err) => res.json({"message": err}))
+  }
 }
 ,
 app.get("/managers",function (req,res) {
@@ -50,8 +85,40 @@ app.get("/managers",function (req,res) {
     .then((data) => res.json(data))
     .catch((err)=> res.json({"message": err}))
     }
+    ,
+    app.post("/images/add", upload.single("imageFile"), function(req, res){
+      res.redirect("/images")
+    
+    }
+,
+app.get("/images",  function(req, res){
+
+  fs.readdir("./public/images/uploaded", function(err, items){
+
+
+     res.send("ITEMS : " + items)
+
+     if (err)
+     console.log(err);
+
+  })
+
+}
+,
+app.post('/employees/add', function(req, res) {
+  data.addEmployee(req.body)
+      .then(res.redirect('/employees'))
+      .catch((err) => res.json({"message": err}))   
+}
+,
+app.get('/employee/:employeeNum', (req, res) => {
+  data.getEmployeesByNum(req.params.employeeNum)
+  .then((data) => {
+      res.json(data);
+  })
+}
   
-)))));
+)))))))))));
 
 app.use((req, res) => {
   res.status(404).send("error 404 page not found!");
